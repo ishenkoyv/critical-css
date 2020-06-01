03209346d417069bd0c420bfef3a4e8f3e230587
Yuriy Ishchenko <yuriy.ishchenko@safetysys.com.au>
2020-06-01 22:29:03 +0300

    Replaced symfony processBuilder with process

--- src/CssGenerators/CriticalGenerator.php
+++ src/CssGenerators/CriticalGenerator.php
@@ -2,7 +2,7 @@
 
 namespace Alfheim\CriticalCss\CssGenerators;
 
-use Symfony\Component\Process\ProcessBuilder;
+use Symfony\Component\Process\Process;
 use Alfheim\CriticalCss\Storage\StorageInterface;
 use Alfheim\CriticalCss\HtmlFetchers\HtmlFetcherInterface;
 
@@ -86,34 +86,36 @@ class CriticalGenerator implements CssGeneratorInterface
     {
         $html = $this->htmlFetcher->fetch($uri);
 
-        $builder = new ProcessBuilder;
-
-        $builder->setPrefix($this->criticalBin);
-
-        $builder->setArguments([
+        $arguments = [
             '--base='.realpath(__DIR__.'/../.tmp'),
             '--width='.$this->width,
             '--height='.$this->height,
             '--minify',
-        ]);
-
+        ];
         if (!is_null($this->timeout)) {
-            $builder->setTimeout($this->timeout);
-
-            $builder->add('--timeout='.$this->timeout);
+            $arguments[] = '--timeout='.$this->timeout;
         }
 
         foreach ($this->css as $css) {
-            $builder->add('--css='.$css);
+            $arguments[] = '--css='.$css;
         }
 
         foreach ($this->ignore as $ignore) {
-            $builder->add('--ignore='.$ignore);
+            $arguments[] = '--ignore='.$ignore;
         }
 
-        $builder->setInput($html);
+        $process = new Process(
+            array_merge(
+                [$this->criticalBin,],
+                $arguments
+            )
+        );
 
-        $process = $builder->getProcess();
+        if (!is_null($this->timeout)) {
+            $process->setTimeout($this->timeout);
+        }
+
+        $process->setInput($html);
 
         $process->run();
 
